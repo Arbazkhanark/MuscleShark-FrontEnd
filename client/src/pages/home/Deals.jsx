7d26c6a6a807cf2e50e679d3d8442e83617d2681
Arbaaz Khan <arbaazkhanark23@gmail.com>
2023-10-14 15:00:47 +0530

    responsive

--- client/src/pages/home/Deals.jsx
+++ client/src/pages/home/Deals.jsx
@@ -2,9 +2,12 @@
 const Deals = () => {
   return (
     <div className="p-4 mt-14">
+        <div className=" w-36">
+            <img src="/muscleshark/temp1.png" alt="img" />
+        </div>
         <div>
-            <img src="" alt="" />
-            <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
         </div>
     </div>
   )
--- client/src/pages/home/OverviewProducts.jsx
+++ client/src/pages/home/OverviewProducts.jsx
@@ -3,46 +3,46 @@ const OverviewProducts = () => {
   return (
     <div className="lg:p-3">
         <div className="flex justify-between">
-            <div className="flex items-center">
+        <div className="flex items-center lg:flex-nowrap  flex-wrap">
                 <img src="/muscleshark/ms products_page-0006.jpg" width={"80px"} alt="" />
                 <div>
-                    <a href="" className=" text-lg font-bold">Mass Gainer</a>
-                    <p className=" text-slate-600">6 products</p>
+                    <a href="" className="lg:text-lg lg:font-bold md:font-medium sm:text-sm max-sm:text-sm">Mass Gainer</a>
+                    <p className="lg:block text-slate-600 sm:hidden max-sm:hidden">6 products</p>
                 </div>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center flex-wrap lg:flex-nowrap">
                 <img src="/muscleshark/ms products_page-0008.jpg" width={"80px"} alt="" />
                 <div>
-                    <a href="" className=" text-lg font-semibold">Whey Protein</a>
-                    <p className=" text-slate-600">6 products</p>
+                    <a href="" className=" lg:text-lg lg:font-bold md:font-medium sm:text-sm max-sm:text-sm">Whey Protein</a>
+                    <p className="lg:block text-slate-600 sm:hidden max-sm:hidden">6 products</p>
                 </div>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center lg:flex-nowrap  flex-wrap">
                 <img src="/muscleshark/ms products_page-0009.jpg" width={"80px"} alt="" />
                 <div>
-                    <a href="" className=" text-lg font-semibold">Pre Workout</a>
-                    <p className=" text-slate-600">6 products</p>
+                    <a href="" className="lg:text-lg lg:font-bold md:font-medium sm:text-sm max-sm:text-sm">Pre Workout</a>
+                    <p className="lg:block text-slate-600 sm:hidden max-sm:hidden">6 products</p>
                 </div>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center lg:flex-nowrap  flex-wrap">
                 <img src="/muscleshark/ms products_page-0011.jpg" width={"80px"} alt="" />
                 <div>
-                    <a href="" className=" text-lg font-semibold">Fat Loss</a>
-                    <p className=" text-slate-600">6 products</p>
+                    <a href="" className="lg:text-lg lg:font-bold md:font-medium sm:text-sm max-sm:text-sm">Fat Loss</a>
+                    <p className="lg:block text-slate-600 sm:hidden max-sm:hidden">6 products</p>
                 </div>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center lg:flex-nowrap  flex-wrap">
                 <img src="/muscleshark/ms products_page-0013.jpg" width={"80px"} alt="" />
                 <div>
-                    <a href="" className=" text-lg font-semibold">Multivitamins</a>
-                    <p className=" text-slate-600">6 products</p>
+                    <a href="" className="lg:text-lg lg:font-bold md:font-medium sm:text-sm max-sm:text-sm">Multivitamins</a>
+                    <p className="lg:block text-slate-600 sm:hidden max-sm:hidden">6 products</p>
                 </div>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center lg:flex-nowrap  flex-wrap">
                 <img src="/muscleshark/ms products_page-0014.jpg" width={"80px"} alt="img" />
                 <div>
-                    <a href="" className=" text-lg font-semibold">Sports & Health</a>
-                    <p className=" text-slate-600">6 products</p>
+                    <a href="" className="lg:text-lg lg:font-bold md:font-medium sm:text-sm max-sm:text-sm">Sports & Health</a>
+                    <p className="lg:block text-slate-600 sm:hidden max-sm:hidden">6 products</p>
                 </div>
             </div>
         </div>
